8118939708eb53415adaa27d106f54a1e9e848ab
zBereczki <28792564+zBereczki@users.noreply.github.com>
2017-08-09 10:43:10 +0200

    added citeproc for citations and toc for table of contents

--- pandoc-docx.sh
+++ pandoc-docx.sh
@@ -2,4 +2,4 @@
 PATH=$PATH:/usr/local/bin
 
 cd "$(dirname "$BB_DOC_PATH")"
-pandoc -o "${BB_DOC_PATH%%.*}".docx "$BB_DOC_PATH"
+pandoc --filter pandoc-citeproc --toc --toc-depth=2 -o "${BB_DOC_PATH%%.*}".docx "$BB_DOC_PATH"
